261c4b8defc16e2fec7b13ba9cb08a43b7fb42e6
Prajwal K <prajwalkreddy17@gmail.com>
2024-12-07 23:54:59 +0530

    added icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import Navbar from "./Components/Navbar"
+import Coding from "../src/assets/Icons/coding.svg"
+import Github from "../src/assets/Icons/github.svg"
+import LinkedIn from "../src/assets/Icons/linkedin.svg"
+import Mail from "../src/assets/Icons/mail.svg"
+import X from "../src/assets/Icons/x.svg"
 
 const App = () => {
   return (
@@ -17,11 +22,24 @@ const App = () => {
           <div className="w-full text-center text-2xl ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nostrum explicabo amet est adipisci eveniet earum ullam, voluptates, beatae quia blanditiis veniam saepe doloribus? Soluta repellat architecto ad quae fugit?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, voluptas itaque. Ullam quia, cum incidunt in quod illum laborum possimus, aliquid sint, eius architecto sequi aliquam modi quam placeat mollitia!
           </div>
-          <div>
-
+          <div className="h-20 w-32">
+            <img src={Coding} />
           </div>
-          <div>
-
+          <div className="">
+            <div className="flex justify-center items-centers gap-5 bg-neutral-200/60 px-4 py-4 rounded-3xl">
+              <a href="" className="hover:cursor-pointer ">
+                <img src={Github} alt="" className="size-7  text-neutral-500" />
+              </a>
+              <a href="" className="hover:cursor-pointer">
+                <img src={Mail} alt="" className="size-7" />
+              </a>
+              <a href="" className="hover:cursor-pointer">
+                <img src={X} alt="" className="size-7" />
+              </a>
+              <a href="" className="hover:cursor-pointer">
+                <img src={LinkedIn} alt="" className="size-7" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function handleMenuOpen() {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
-    <section className=" mx-32 px-7 py-9 flex justify-between">
+    <section className="mx-32 px-7 py-9 flex justify-between">
       <div className="font-motterdam text-3xl font-extralight">
         Prajwal
       </div>
-      <div>
-        Settings
+      <div onClick={handleMenuOpen}>
+        {menuOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6 transition duration-1000 ease-in"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6 transition duration-1000 ease-in"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+          </svg>
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-
-export default Navbar
+export default Navbar;
